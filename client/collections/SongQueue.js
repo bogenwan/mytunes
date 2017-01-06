45a18d44795047078526b907aaf529f91cf84434
bogenwan <pobo1230@gmail.com>
2017-01-05 18:53:54 -0800

    Pass SongModel, SongQueue, and SongQueueView tests

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,28 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
+    this.on('add', function() {
+      // Convert condition to backbone version later
+      if (this.models.length === 1) {
+        this.playFirst();
+      }
+    });
+
+    this.on('ended', this.dequeue);
+    this.on('dequeue', this.dequeue);
+
+  },
+  
+  playFirst: function() {
+    this.at(0).play();
+  },
+
+  dequeue: function() {
+    this.remove(this.at(0));
+    
+    if (this.length > 0) {
+      this.playFirst();
+    }
   }
 
 });
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,11 +1,34 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
 
+  // model: SongQueue,
+
   initialize: function() {
+    console.log(this);
+    
+    
+    console.log(this.songQueueEntryView);
+    // this.fakesongs.on('add', () => console.log('hi!'));
+    // this.on('add', this.$el.render);
+
+    this.collection.on('add', this.render, this);
+    this.collection.on('remove', this.render, this);
+
+
   },
 
+  // events: {
+  //   'add': 'render',
+  // },
+
   render: function() {
+    this.songQueueEntryView = new SongQueueEntryView();
+    this.songQueueEntryView.render();
     return this.$el;
+
+    // From solution video:
+    // return this.$el.html(this.template(this.model.attributes))
+
   }
 
 });
