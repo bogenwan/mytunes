09c8e52def32b21566d9991af9f6e97b37c28d06
bogenwan <pobo1230@gmail.com>
2017-01-05 22:05:05 -0800

    Pass libraryEntryView tests.

--- client/views/SongQueueEntryView.js
+++ client/views/SongQueueEntryView.js
@@ -1,4 +1,19 @@
 // SongQueueEntryView.js - Defines a backbone view class for the song queue entries.
 var SongQueueEntryView = Backbone.View.extend({
-  // your code here!
+  tagName: 'tr',
+
+  template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'),
+
+  // Not sure if we need this yet...
+  // events: {
+  //   'click': function() {
+  //     // this.model.dequeue();
+
+  //   }
+  // },
+
+  render: function() {
+    // this.songQueueEntryView = new SongQueueEntryView({model: song}).render();
+    // return this.$el.html(this.template(this.model.attributes));
+  }
 });
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,29 +1,22 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
 
-  // model: SongQueue,
+  tagName: 'table',
 
-  initialize: function() {
-    console.log(this);
-    
-    
-    console.log(this.songQueueEntryView);
-    // this.fakesongs.on('add', () => console.log('hi!'));
-    // this.on('add', this.$el.render);
-
+  initialize: function() {   
+    this.render();
     this.collection.on('add', this.render, this);
     this.collection.on('remove', this.render, this);
 
-
   },
 
-  // events: {
-  //   'add': 'render',
-  // },
-
   render: function() {
-    this.songQueueEntryView = new SongQueueEntryView();
-    this.songQueueEntryView.render();
+    // this.$el.children().detach();
+
+    this.$el.html('<th>Queue</th>');
+    // .append(
+    this.songQueueEntryView = new SongQueueEntryView().render();
+    // );
     return this.$el;
 
     // From solution video:
